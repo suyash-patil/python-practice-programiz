26313f356ea76351301c88ca41341f019e02a316
suyash-patil <suyashpatil532001@gmail.com>
2023-01-14 23:09:03 +0530

    added swapping program

---
+++ 006.py
@@ -0,0 +1,7 @@
+a,b = map(int, input().split())
+
+print('The values of a is {0} and b is {1} before swapping'.format(a, b))
+
+a,b = b,a
+
+print('The values of a is {0} and b is {1} after swapping'.format(a, b))
